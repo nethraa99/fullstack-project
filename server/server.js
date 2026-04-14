6885f337b6b121ae3136d297031adc66d4f1b85c
nethraa99 <nethraasai87@gmail.com>
2026-04-14 23:10:09 +0530

    fix port issue

--- server/server.js
+++ server/server.js
@@ -1,25 +1,15 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+    import express from "express";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
-});
-
-app.get("/api/data", (req, res) => {
-  res.json({ message: "Hello from backend!" });
+      res.send("Server is running 🚀");
 });
 
 app.get("/api", (req, res) => {
   res.json({ message: "Backend working 🚀" });
-}
-);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
